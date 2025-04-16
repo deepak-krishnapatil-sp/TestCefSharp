28ad16a748f7b12be10b557dc7b5b6edb8c30fbc
deepak-krishnapatil-sp <deepak.krishnapatil@sailpoint.com>
2025-04-16 16:18:08 +0530

    remove unwanted event handlers

--- TestCefSharp/BrowserForm.cs
+++ TestCefSharp/BrowserForm.cs
@@ -93,18 +93,27 @@ namespace TestCefSharp.WinForms
                 MessageBox.Show("IsBrowserInitializedChanged event not found.", "Event Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
-            AddEventHandler("LoadError",           "CefSharp.LoadErrorEventArgs",           nameof(OnBrowserLoadError));
-            AddEventHandler("TitleChanged",        "CefSharp.TitleChangedEventArgs",        nameof( OnBrowserTitleChanged));
-            AddEventHandler("StatusMessage",       "CefSharp.StatusMessageEventArgs",       nameof(OnBrowserStatusMessage));
-            AddEventHandler("ConsoleMessage",      "CefSharp.ConsoleMessageEventArgs",      nameof(OnBrowserConsoleMessage));
-            AddEventHandler("AddressChanged",      "CefSharp.AddressChangedEventArgs",      nameof(OnBrowserAddressChanged));
-            AddEventHandler("LoadingStateChanged", "CefSharp.LoadingStateChangedEventArgs", nameof(OnLoadingStateChanged));
+            AddEventHandler("LoadError",
+                            "CefSharp.LoadErrorEventArgs",
+                            nameof(OnBrowserLoadError));
 
+            AddEventHandler("AddressChanged",
+                            "CefSharp.AddressChangedEventArgs",
+                            nameof(OnBrowserAddressChanged));
 
-            AddEventHandler("FrameLoadEnd",         "CefSharp.FrameLoadEndEventArgs",       nameof(OnFrameLoadEnd));
-            AddEventHandler("JavascriptMessageReceived", "CefSharp.JavascriptMessageReceivedEventArgs", nameof(OnJavascriptMessageReceived));
+            AddEventHandler("FrameLoadStart",
+                            "CefSharp.FrameLoadStartEventArgs",
+                            nameof(OnFrameLoadStart));
 
+            AddEventHandler("FrameLoadEnd",
+                            "CefSharp.FrameLoadEndEventArgs",
+                            nameof(OnFrameLoadEnd));
+
+            AddEventHandler("JavascriptMessageReceived",
+                            "CefSharp.JavascriptMessageReceivedEventArgs",
+                            nameof(OnJavascriptMessageReceived));
         }
+
         private void ExitMenuItemClick(object sender, EventArgs e)
         {
             MethodInfo disposeMethod = browserType.GetMethod("Dispose");
@@ -198,63 +207,6 @@ namespace TestCefSharp.WinForms
             }
         }
 
-        private void OnBrowserTitleChanged(object sender, object args)
-        {
-            try
-            {
-                // Get the Address property from AddressChangedEventArgs
-                Type titleChangedEventArgsType = cefSharpAssembly.GetType("CefSharp.TitleChangedEventArgs");
-                PropertyInfo addressProperty = titleChangedEventArgsType.GetProperty("Title");
-                string newTitle = (string)addressProperty.GetValue(args);
-
-                this.InvokeOnUiThreadIfRequired(() => Text = title + " - " + newTitle);
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"ChromiumWebBrowser:Error handling OnBrowserTitleChanged: {ex.Message}");
-                MessageBox.Show($"Error handling OnBrowserTitleChanged: {ex.Message}", "Event Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void OnBrowserStatusMessage(object sender, object args)
-        {
-            try
-            {
-                Type statusMessageEventArgsType = cefSharpAssembly.GetType("CefSharp.StatusMessageEventArgs");
-                PropertyInfo valueProperty = statusMessageEventArgsType.GetProperty("Value");
-
-                string statusText = (string)valueProperty.GetValue(args);
-                return;
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"ChromiumWebBrowser:Error handling StatusMessage: {ex.Message}");
-                MessageBox.Show($"Error handling StatusMessage: {ex.Message}", "Event Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void OnBrowserConsoleMessage(object sender, object args)
-        {
-            try
-            {
-                Type consoleMessageEventArgsType = cefSharpAssembly.GetType("CefSharp.ConsoleMessageEventArgs");
-                PropertyInfo messageProperty = consoleMessageEventArgsType.GetProperty("Message");
-                PropertyInfo sourceProperty = consoleMessageEventArgsType.GetProperty("Source");
-                PropertyInfo lineProperty = consoleMessageEventArgsType.GetProperty("Line");
-
-                string message = (string)messageProperty.GetValue(args);
-                string source = (string)sourceProperty.GetValue(args);
-                int line = (int)lineProperty.GetValue(args);
-
-                //DisplayOutput(string.Format("Line: {0}, Source: {1}, Message: {2}", line, source, message));
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"ChromiumWebBrowser:Error handling ConsoleMessage: {ex.Message}");
-                MessageBox.Show($"Error handling ConsoleMessage: {ex.Message}", "Event Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
         private void OnBrowserAddressChanged(object sender, object args)
         {
             try
@@ -274,51 +226,9 @@ namespace TestCefSharp.WinForms
             }
         }
 
-        private void OnLoadingStateChanged(object sender, object args)
+        private void OnFrameLoadStart(object sender, object args)
         {
-            try
-            {
-                // Get the LoadingStateChangedEventArgs properties
-                Type loadingStateChangedEventArgsType = cefSharpAssembly.GetType("CefSharp.LoadingStateChangedEventArgs");
-                PropertyInfo canReloadProperty = loadingStateChangedEventArgsType.GetProperty("CanReload");
-                //PropertyInfo canGoBackProperty = loadingStateChangedEventArgsType.GetProperty("CanGoBack");
-                //PropertyInfo canGoForwardProperty = loadingStateChangedEventArgsType.GetProperty("CanGoForward");
-
-                bool canReload = (bool)canReloadProperty.GetValue(args);
-                //bool canGoBack = (bool)canGoBackProperty.GetValue(args);
-                //bool canGoForward = (bool)canGoForwardProperty.GetValue(args);
-
-                //this.InvokeOnUiThreadIfRequired(() => SetIsLoading(!canReload));
-            }
-            catch (Exception ex)
-            {
-                Logger.Error($"ChromiumWebBrowser:Error handling LoadingStateChanged: {ex.Message}");
-                MessageBox.Show($"Error handling LoadingStateChanged: {ex.Message}", "Event Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void AddEventHandler(string eventName, string eventArgsTypeStr, string eventHandlerFuncName)
-        {
-            //browser.AddressChanged += OnBrowserAddressChanged;
-            EventInfo eventInfo = browserType.GetEvent(eventName);
-            if (eventInfo != null)
-            {
-                // Create an adapter delegate that matches the expected signature
-                Type eventArgsType = cefSharpAssembly.GetType(eventArgsTypeStr);
-                Type handlerType = typeof(EventHandler<>).MakeGenericType(eventArgsType);
-                MethodInfo adapterMethod = typeof(BrowserForm).GetMethod(eventHandlerFuncName, BindingFlags.Instance | BindingFlags.NonPublic);
-                Delegate handler = Delegate.CreateDelegate(handlerType, this, adapterMethod);
-                // Add the handler to the event
-                eventInfo.AddEventHandler(browser, handler);
-            }
-            else
-            {
-                Logger.Error("ChromiumWebBrowser: {0} event not found.", eventName);
-
-                string errorString = "ChromiumWebBrowser: " + eventName + " event not found.";
-                MessageBox.Show(errorString, "Event Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-
+            return;
         }
 
         private async void OnFrameLoadEnd(object sender, object args)
@@ -386,7 +296,7 @@ namespace TestCefSharp.WinForms
 
             await ExecuteJavaScript(successScript);
         }
-
+        
         private void OnJavascriptMessageReceived(object sender, object args)
         {
             try
@@ -422,6 +332,30 @@ namespace TestCefSharp.WinForms
                 Logger.Error("Exception in OnJavascriptMessageReceived: " + ex.ToString());
             }
         }
+        
+        private void AddEventHandler(string eventName, string eventArgsTypeStr, string eventHandlerFuncName)
+        {
+            //browser.AddressChanged += OnBrowserAddressChanged;
+            EventInfo eventInfo = browserType.GetEvent(eventName);
+            if (eventInfo != null)
+            {
+                // Create an adapter delegate that matches the expected signature
+                Type eventArgsType = cefSharpAssembly.GetType(eventArgsTypeStr);
+                Type handlerType = typeof(EventHandler<>).MakeGenericType(eventArgsType);
+                MethodInfo adapterMethod = typeof(BrowserForm).GetMethod(eventHandlerFuncName, BindingFlags.Instance | BindingFlags.NonPublic);
+                Delegate handler = Delegate.CreateDelegate(handlerType, this, adapterMethod);
+                // Add the handler to the event
+                eventInfo.AddEventHandler(browser, handler);
+            }
+            else
+            {
+                Logger.Error("ChromiumWebBrowser: {0} event not found.", eventName);
+
+                string errorString = "ChromiumWebBrowser: " + eventName + " event not found.";
+                MessageBox.Show(errorString, "Event Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
 
         private string generateNonce()
         {
@@ -432,6 +366,7 @@ namespace TestCefSharp.WinForms
             }
             return BitConverter.ToString(nonceBytes).Replace("-", "").ToLower();
         }
+
         private async Task<bool> ExecuteJavaScript(string script)
         {
             try
@@ -557,6 +492,7 @@ namespace TestCefSharp.WinForms
                 Logger.Error("Error parsing JSON: " + ex.Message);
             }
         }
+
         private async Task<bool> VerifyNonce(string url)
         {
             try
